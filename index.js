1f02512c2232852816510aab364efca9d28bb970
Ahnaf <ahnafanis0@gmail.com>
2024-01-07 20:48:45 +0600

    init factories

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+const player = (sign) => {
+    const getSign = () => sign;
+    return { getSign };
+};
+
+const gameboard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+})();
