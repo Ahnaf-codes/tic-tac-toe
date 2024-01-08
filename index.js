306c78fdd2478b0d9be4bbcb4b7d6fab9a611cd7
Ahnaf <ahnafanis0@gmail.com>
2024-01-08 21:25:39 +0600

    added box event ;isteners

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ const gameboard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
     const setBox = (i, sign) => {
         board[i] = sign;
+
     };
-    return { board, setBox };
+    const getBoard = () => board;
+    return { getBoard, setBox };
 })();
 
 const displayController = (() => {
+    //const gameboard = document.getElementById('board');
+    const boxes = document.querySelectorAll('.box');
+    const player1 = player("X");
+    const player2 = player("O");
+    let currentPlayer = player1;
 
+    function togglePlayer() {
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+
+    };
+    boxes.forEach((box, i) => {
+        box.addEventListener("click", () => {
+            gameboard.setBox(i, currentPlayer.getSign());
+            box.innerHTML = currentPlayer.getSign();
+            togglePlayer();
+            console.log("hi");
+        });
+    });
 })();
